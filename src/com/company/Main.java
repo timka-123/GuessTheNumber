2887e2983379f0be64dc8b6f1d1ffb573fde8cd3
Vasya Korghikov <vakzn@yandex.ru>
2021-06-25 11:33:27 +0300

    v1.0
    Add game

---
+++ src/com/company/Main.java
@@ -0,0 +1,27 @@
+package com.company;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Random random = new Random();
+        Scanner scanner = new Scanner(System.in);
+        int counter, guess, number;
+        do {
+            counter = 0;
+            guess = -1;
+            number = random.nextInt(10);
+            while (counter < 3 && guess != number) {
+                System.out.println("Guess the number 0....9: ");
+                guess = scanner.nextInt();
+                if (guess != number) {
+                    System.out.println("You number is " + (guess != number? "less" : "quarter"));
+                    counter++;
+                }
+            }
+            System.out.println("You are " +(guess == number? "WIN" : "lose. Number is " + number));
+            System.out.println("Do you want to coutune?");
+        } while (scanner.nextInt() == 1);
+    }
+}
